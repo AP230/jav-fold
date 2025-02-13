63b14da99690a72db83d237d2ded21a83974721e
ashley <126aperezmorales@frhsd.com>
2025-02-13 14:03:12 -0500

    first commit

---
+++ js/caughtspeeding.js
@@ -0,0 +1,20 @@
+function caught speeding (X,y){
+
+
+  var isBirthday = x;
+  var speed = y;
+
+  if(isbirthday){
+         speed -=5;
+   }
+
+
+
+
+
+
+if(speed <= 60){return 0;}
+elself ( speed >=61 && <=80){return 1;}
+elself (speed >=81){return 2;}
+
+}
